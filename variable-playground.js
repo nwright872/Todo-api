b6c09a88fe29969e9cc16fcc7040468a671e1e62
Nick Wright <nwright872@gmail.com>
2016-07-29 15:50:04 -0400

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//   name: " Nick",
+//   age: 21
+// };
+//
+// function updatePerson (obj) {
+//   // obj = {
+//   //   name : 'Nick',
+//   //   age : 23
+//   // };
+//   person.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+
+//Array Example
+var grade = [15, 27];
+
+ function addGrade(gradeArr) {
+   gradeArr.push(25);
+   debugger;
+  // gradesArr = [12, 33, 99];
+
+ }
+ addGrade(grade);
+ console.log(grade);
